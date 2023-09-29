e51f06a574ae024f5e5f896fd85a118f91f9c9dd
dagnisiljins <dagnis.iljins@gmail.com>
2023-09-29 18:39:27 +0300

    Papildinājumi

--- Uzdevums/01_uzdevums.php
+++ Uzdevums/01_uzdevums.php
@@ -1,4 +1,10 @@
 <?php
+// 1) Šajā piemērā izmantota curl metode - visbiežāk lietotā php.
+// Citas metodes:
+// 2) Guzzle metode (guzzlephp), jāinstalē paplašinājumi
+// 3) file_get_contents metode
+// 4) AWS SDK for PHP (arī jāinstalē), bet nevisiem API šis strādās, jāskatās individuāli (STRIPE ir šāds). Vieglāk lietojams.
+
 
 function fetchEpisodeData($url) {
     $ch = curl_init($url);
@@ -6,11 +12,11 @@ function fetchEpisodeData($url) {
 // nākamā rinda ar curl_setop ir ielikta manuāli, lai norādītu kur pieejams fails cacer.pem
     curl_setopt($ch, CURLOPT_CAINFO, 'C:/CA certificates/cacert.pem'); // sertifikātu /cacert.pem es manuāli ieliku un tad aizgāja
 
-    $response = curl_exec($ch);
-    curl_close($ch);
+    $response = curl_exec($ch);// Ar šo mēs uzsākam execution of curl
+    curl_close($ch); //aizveram, lai taupītu resursus, ko izmanto
 
     if ($response) {
-        return json_decode($response, true);
+        return json_decode($response, true);//ar curl iegūstam json info, ko nepieciešams dekodēt
     } else {
         echo "Failed to fetch data from URL: $url" . PHP_EOL;
         return null;
--- Uzdevums/02.php
+++ Uzdevums/02.php
@@ -1,5 +1,7 @@
 <?php
 // Lekciju laikā izveidotais variants - īsais variants
+// Šeit izmantota php funkcija file_get_contents, šai metodei ir mīnuss, ka, ja neizpildās pieprasījums, tad netiek sūtīts atpakaļ info, kādi bugi jālabo
+
 
 $episodes = json_decode(file_get_contents('https://rickandmortyapi.com/api/episode'));
 
