cec5819a760ec8e63660d9bc1a478661683c987c
dagnisiljins <dagnis.iljins@gmail.com>
2023-10-05 12:32:38 +0300

    Papildinājumi

--- classes-and-objects/04.php
+++ classes-and-objects/04.php
@@ -1,54 +1,45 @@
 <?php
-//
-class Movie {
+//Movie - full task in read me
+class Movie
+{
     private $title;
     private $studio;
     private $rating;
 
-    public function __construct($title, $studio, $rating) {
+    public function __construct($title, $studio, $rating)
+    {
         $this->title = $title;
         $this->studio = $studio;
         $this->rating = $rating;
     }
 
-    public function getTitle() {
-        return $this->title;
-    }
-
-    public function getStudio() {
-        return $this->studio;
-    }
-
-    public function getRating() {
-        return $this->rating;
-    }
-
     public static function GetPG($movies) {
         $pgMovies = [];
+
         foreach ($movies as $movie) {
-            if ($movie->rating === "PG") {
+            if ($movie->rating === 'PG') {
                 $pgMovies[] = $movie;
             }
         }
         return $pgMovies;
     }
+
+    public function getInfo() {
+        return $this->title . ', ' . $this->studio . ', ' . $this->rating;
+    }
+
 }
 
-// Create instances of the Movie class
-$movie1 = new Movie("Casino Royale", "Eon Productions", "PG-13");
-$movie2 = new Movie("Glass", "Buena Vista International", "PG-13");
-$movie3 = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+$movieOne = new Movie('Casino Royale', 'Eon Productions', 'PG13');
+$movieTwo = new Movie('Glass', 'Buena Vista International', 'PG13');
+$movieThree = new Movie('Spider-Man: Into the Spider-Verse', 'Columbia Pictures', 'PG');
 
-// Create an array of Movie instances
-$movies = [$movie1, $movie2, $movie3];
+$movies = [$movieOne, $movieTwo, $movieThree];
 
-// Get PG-rated movies from the array
 $pgMovies = Movie::GetPG($movies);
 
-// Print the PG-rated movies
 foreach ($pgMovies as $pgMovie) {
-    echo "Title: " . $pgMovie->getTitle() . "\n";
-    echo "Studio: " . $pgMovie->getStudio() . "\n";
-    echo "Rating: " . $pgMovie->getRating() . "\n";
-    echo "-------------------------\n";
+    echo $pgMovie->getInfo();
 }
+
+
